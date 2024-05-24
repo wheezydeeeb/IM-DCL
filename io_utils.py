9ffe7f356f565c355c86eab288fb694d7442628b
wheezydeeeb <tridibjdas@gmail.com>
2024-05-24 18:30:35 +0000

    Updated configs.py

--- io_utils.py
+++ io_utils.py
@@ -17,7 +17,7 @@ def parse_args(script):
     parser.add_argument('--alpha', default=0.5, type=float)
 
     if script == 'finetune':
-        parser.add_argument('--dtarget', default='CropDisease', choices=['CropDisease', 'EuroSAT', 'ISIC', 'ChestX', 'miniImageNet'])
+        parser.add_argument('--dtarget', default='CropDisease', choices=['CropDisease', 'EuroSAT', 'ISIC', 'ChestX', 'miniImageNet', 'FER2013'])
         parser.add_argument('--test_n_way', default=5, type=int, help='class num to classify for testing')
         parser.add_argument('--n_shot', default=5, type=int, help='number of labeled data in each class, same as n_support')
     else:
