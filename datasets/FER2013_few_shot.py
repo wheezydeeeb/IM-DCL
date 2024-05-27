07f982a6aa2e47d6dcfe0b5b4099016d3e5fd703
wheezydeeeb <tridibjdas@gmail.com>
2024-05-27 15:13:43 +0000

    Updated datasets/FER2013_few_shot.py

--- datasets/FER2013_few_shot.py
+++ datasets/FER2013_few_shot.py
@@ -1,4 +1,4 @@
- # This code is modified from https://github.com/facebookresearch/low-shot-shrink-hallucinate
+# This code is modified from https://github.com/facebookresearch/low-shot-shrink-hallucinate
 
 import torch
 from PIL import Image
@@ -18,6 +18,7 @@ sys.path.append("../")
 from configs import *
 
 identity = lambda x:x
+
 class SimpleDataset:
     def __init__(self, transform, target_transform=identity):
         self.transform = transform
@@ -45,65 +46,46 @@ class SimpleDataset:
         return len(self.meta['image_names'])
 
 class SetDataset:
-    def __init__(self, batch_size, transform):
-
-        self.sub_meta = {}
-        self.cl_list = range(7)
-
-        for cl in self.cl_list:
-            self.sub_meta[cl] = []
+    def __init__(self, n_way, n_samples, transform):
+        self.n_way = n_way
+        self.n_samples = n_samples
+        self.transform = transform
 
         d = ImageFolder(FER2013_path)
-
+        self.data = []
+        self.labels = []
         for i, (data, label) in enumerate(d):
-            self.sub_meta[label].append(data)
+            self.data.append(data)
+            self.labels.append(label)
 
-        for key, item in self.sub_meta.items():
-            print (len(self.sub_meta[key]))
-    
-        self.sub_dataloader = [] 
-        sub_data_loader_params = dict(batch_size = batch_size,
-                                  shuffle = True,
-                                  num_workers = 0, #use main thread only or may receive multiple batches
-                                  pin_memory = False)        
-        for cl in self.cl_list:
-            sub_dataset = SubDataset(self.sub_meta[cl], cl, transform = transform )
-            self.sub_dataloader.append( torch.utils.data.DataLoader(sub_dataset, **sub_data_loader_params) )
-
-    def __getitem__(self, i):
-        return next(iter(self.sub_dataloader[i]))
+    def __getitem__(self, batch):
+        x = []
+        for way in batch:
+            way_data = []
+            for i in range(self.n_way):
+                way_data.append(self.transform(self.data[self.labels.index(i)][way]))
+            x.append(torch.stack(way_data))
+        x = torch.stack(x)
+        return x
 
     def __len__(self):
-        return len(self.sub_dataloader)
-
-class SubDataset:
-    def __init__(self, sub_meta, cl, transform=transforms.ToTensor(), target_transform=identity):
-        self.sub_meta = sub_meta
-        self.cl = cl 
-        self.transform = transform
-        self.target_transform = target_transform
-
-    def __getitem__(self,i):
-
-        img = self.transform(self.sub_meta[i])
-        target = self.target_transform(self.cl)
-        return img, target
-
-    def __len__(self):
-        return len(self.sub_meta)
+        return len(self.labels)
 
 class EpisodicBatchSampler(object):
-    def __init__(self, n_classes, n_way, n_episodes):
+    def __init__(self, n_classes, n_way, n_episodes, n_shot, n_query):
         self.n_classes = n_classes
         self.n_way = n_way
         self.n_episodes = n_episodes
+        self.n_shot = n_shot
+        self.n_query = n_query
 
     def __len__(self):
         return self.n_episodes
 
     def __iter__(self):
         for i in range(self.n_episodes):
-            yield torch.randperm(self.n_classes)[:self.n_way]
+            batch = torch.randperm(self.n_classes)[:self.n_way]
+            yield batch
 
 class TransformLoader:
     def __init__(self, image_size, 
@@ -166,15 +148,16 @@ class SetDataManager(DataManager):
         super(SetDataManager, self).__init__()
         self.image_size = image_size
         self.n_way = n_way
-        self.batch_size = n_support + n_query
+        self.n_support = n_support
+        self.n_query = n_query
         self.n_eposide = n_eposide
 
         self.trans_loader = TransformLoader(image_size)
 
     def get_data_loader(self, aug):
         transform = self.trans_loader.get_composed_transform(aug)
-        dataset = SetDataset(self.batch_size, transform)
-        sampler = EpisodicBatchSampler(len(dataset), self.n_way, self.n_eposide )  
+        dataset = SetDataset(self.n_way, self.n_support + self.n_query, transform)
+        sampler = EpisodicBatchSampler(len(dataset), self.n_way, self.n_eposide, self.n_support, self.n_query)  
         data_loader_params = dict(batch_sampler = sampler,  num_workers = 2, pin_memory = True)       
         data_loader = torch.utils.data.DataLoader(dataset, **data_loader_params)
         return data_loader
