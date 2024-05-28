8acfcc93fd7d2f22be1b9815f53fa6081727d788
wheezydeeeb <tridibjdas@gmail.com>
2024-05-28 09:32:46 +0000

    Updated adapt_da.py to default settings.

--- adapt_da.py
+++ adapt_da.py
@@ -147,7 +147,6 @@ def finetune(name, novel_loader, n_query=15, freeze_backbone=False, n_way=5, n_s
             y_var_tmp = Variable(torch.from_numpy(np.repeat(range(n_way), (n_support+n_query)))).to('cuda')
             
             #y_var_tmp = Variable(torch.from_numpy(np.repeat(range(7), (n_support+n_query)))).to('cuda')
-            print(f"x_var shape: {x_var[:, n_support:, :, :, :].shape}")
             x_b_i.append(x_var[:, n_support:, :, :, :].contiguous().view(n_way * n_query, *x.size()[2:]))
             x_a_i_tmp = x_var[:, :n_support, :, :, :].contiguous().view(n_way * n_support, *x.size()[2:])
             if aug == 0:
@@ -213,7 +212,7 @@ def finetune(name, novel_loader, n_query=15, freeze_backbone=False, n_way=5, n_s
 
                 output = pretrained_model(z_batch)
                 print(output.shape)
-                output = output.permute(0, 3, 2, 1)
+                # output = output.permute(0, 3, 2, 1)
                 print(output.size())
                 outputs = classifier(output)
                 print(y_batch.size())
